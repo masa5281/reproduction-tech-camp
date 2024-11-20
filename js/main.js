c090596228bf71c769d32eb0d50bdf28a4a44a4b
masa5281 <masa.h.5281@gmail.com>
2024-11-20 21:40:58 +0900

    Add:スキルセクションのPC版デザインをCSSで実装完了
    Add:新規JavaScriptファイルを追加

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// スライダー
+$(function() {
+  $(".skills__list").slick({
+    autoplay: true,
+    autoplaySpeed: 3000, //自動再生時に次が動くまでの時間
+    speed: 500, //スライドが流れる速度（画像の端から端まで）
+    slidesToShow: 5, //表示するスライドの数
+    arrows: true,
+    infinite: true,
+    prevArrow: "<button class='slick-prev slick-arrow'></button>",
+    nextArrow: "<button class='slick-next slick-arrow'></button>",
+    centerMode: true,
+    centerPadding: '1%',
+    initialSlide: 2, // スライドの開始位置を変更
+  });
+});
+
+// スクロールでフェードイン
+$(function() {
+  $(".fade-up").on("inview", function() {
+    $(this).addClass("is-inview");
+  });
+}); 
